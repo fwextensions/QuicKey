be6b155164591c7a124ddf4890851d59c91fe673
John Dunning <fw@johndunning.com>
2017-07-24 20:09:23 -0700

    Added /docs directory. Switched to minified ReactJS 0.15.6. Changed Mac suggested_key to Ctrl+Q.

---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# QuicKey
+
+A Chrome extension for switching tabs or pulling up bookmarks and history via a Quicksilver-style search.
+ 
+![QuicKey](../src/img/icon-128.png)
--- src/js/require-config.js
+++ src/js/require-config.js
@@ -1,10 +1,9 @@
 require.config({
-//	urlArgs: "bust=" + +new Date(),
 	waitSeconds: 10,
 	baseUrl: "/js",
 	paths: {
-		react: "lib/react-0.14.3",
-		"react-dom": "lib/react-dom-0.14.3",
+		react: "lib/react-with-addons.min",
+		"react-dom": "lib/react-dom.min",
 		jsx: "lib/jsx",
 		text: "lib/text",
 		JSXTransformer: "lib/JSXTransformer",
