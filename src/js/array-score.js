49ab1b6fd8f0b4177ecc0a9b79cc53a4fd583737
John Dunning <fw@johndunning.com>
2017-09-19 00:06:11 -0700

    Move handling of hitMask Set into quick-score.js
    Simplify array-score.js by moving the conversion of the Set hitMask to an array into quick-score.js.

--- src/js/array-score.js
+++ src/js/array-score.js
@@ -18,14 +18,6 @@ define(function() {
 		}
 
 
-		function compareNumbers(
-			a,
-			b)
-		{
-			return a - b;
-		}
-
-
 		return function scoreArray(
 			items,
 			text)
@@ -46,30 +38,11 @@ define(function() {
 			items.forEach(function(item) {
 					// find the highest score for each keyed string on this item
 				item.score = keyNames.reduce(function(currentScore, key) {
-						// we use a Set for the hitMask so that indexes can only
-						// be added once
-					var hitMask = new Set(),
+					var hitMask = [];
 						newScore = score(item[key], text, hitMask);
 
 					item.scores[key] = newScore;
-
-						// convert the hitMask to an Array, which is easier to
-						// work with.  we have to sort it because the scorer may
-						// find a partial match later in the string, and then
-						// not find the rest of the query, so it starts over with
-						// a shorter piece of the query, which it might find
-						// earlier in the string.  in that case, the hitMask will
-						// have later indexes first, which we slice off so that
-						// the hitMask has at most the same number of indices as
-						// the length of the query, though this might still
-						// contain an extraneous hit.  and we have to sort the
-						// array with a function that correctly sorts numbers,
-						// because JavaScript.  if the score is 0, force the
-						// hitMask to be empty, since it could still have some
-						// partial hits in it, and we don't want to highlight
-						// just parts of the query in a result.
-					item.hitMasks[key] = (newScore == 0) ? [] :
-						Array.from(hitMask).sort(compareNumbers).slice(0, text.length);
+					item.hitMasks[key] = hitMask;
 
 					return Math.max(currentScore, newScore);
 				}, 0);
