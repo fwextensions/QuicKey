1be199bb947ee0b331e97172f918fcbc0ef35cbc
John Dunning <fw@johndunning.com>
2018-12-15 18:17:38 -0800

    Handle new form of TGS tabs in bad state
    When the title is the same as the URL, pull the title from the URL, like for the "Suspended Tab" case.

--- src/js/popup/data/get-tabs.js
+++ src/js/popup/data/get-tabs.js
@@ -10,6 +10,26 @@ define([
 	_
 ) {
 	const TitlePattern = /ttl=([^&]+)/;
+	const BadTGSTitlePattern = /^chrome-extension:\/\/[^/]+\/suspended\.html#ttl=([^&]+)/;
+
+
+	function decode(
+		string)
+	{
+		var result = string;
+
+			// try to use decodeURIComponent() to unescape the ttl param, which
+			// sometimes throws if it doesn't like the encoding
+		try {
+			result = decodeURIComponent(string);
+		} catch (e) {
+			try {
+				result = unescape(string);
+			} catch (e) {}
+		}
+
+		return result;
+	}
 
 
 	return function getTabs(
@@ -40,22 +60,17 @@ define([
 
 						// if the tab is suspended, check if it it's in the bad
 						// state where The Great Suspender hasn't updated its
-						// title so it just says "Suspended Tab".  if so, pull
-						// the title from the ttl param that TGS puts in the URL.
-					if (tab.unsuspendURL && tab.title == "Suspended Tab") {
+						// title so it just says "Suspended Tab".  or in more
+						// recent builds, it sometimes shows the chrome-extension
+						// URL as the title, with spaces instead of %20.  in
+						// these cases, pull the title from the ttl param that
+						// TGS puts in the URL.
+					if (tab.unsuspendURL && (tab.title == "Suspended Tab" ||
+							BadTGSTitlePattern.test(tab.title))) {
 						match = tab.url.match(TitlePattern);
 
 						if (match) {
-								// try to use decodeURIComponent() to unescape
-								// the ttl param, which sometimes throws if it
-								// doesn't like the encoding
-							try {
-								tab.title = decodeURIComponent(match[1]);
-							} catch (e) {
-								try {
-									tab.title = unescape(match[1]);
-								} catch (e) {}
-							}
+							tab.title = decode(match[1]);
 						}
 					}
 				});
