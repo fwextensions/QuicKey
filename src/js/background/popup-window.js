804048d130de6a5f79e414d9ded7db85f803ff57
John Dunning <fw@johndunning.com>
2020-06-14 19:35:05 -0700

    Support persisting the popup as a tab, instead of an off-screen window
    Add type to popupWindow, and use it to either create the popup as a window or a tab and to determine how it's shown/hidden.
    In tab mode, hide the popup as a tab in the last normal window.
    Pass type and focusSearch params on the URL when opening the popup, so it knows how to behave on first launch.
    Don't log errors in popupWindow.close().
    Close all the open tabs/windows when the background is reloaded.
    Don't invert the icon when hitting the next tab command while the user isn't navigating.
    Add openedForSearch member to the popup, so it can set the selected index correctly depending on how the window was opened.  Avoids an extra render.
    Add focusSearch prop to popup.
    Log event.reason.message in error-handler.js, in case it's a getter.

--- src/js/background/popup-window.js
+++ src/js/background/popup-window.js
@@ -16,8 +16,23 @@ define([
 	let popupAdjustmentWidth = 0;
 	let popupAdjustmentHeight = 0;
 	let isVisible = false;
-	let window;
+	let type = "window";
+	let windowID;
+	let tabID;
 	let lastActiveTab;
+
+
+	async function getLastWindow()
+	{
+		const windows = await cp.windows.getAll({
+			windowTypes: ["normal"],
+			populate: true
+		});
+
+			// return the last unfocused window, or just the last one, if
+			// there's only one open and it's focused
+		return windows.filter(({focused}) => !focused).pop() || windows.pop();
+	}
 
 
 	function calcPosition(
@@ -28,33 +43,51 @@ define([
 		const height = PopupInnerHeight + popupAdjustmentHeight;
 		const left = Math.max(0, targetX + Math.floor((targetW - width) / 2));
 		const top = Math.max(0, targetY + Math.floor((targetH - height) / 2));
-
-		return {left, top, width, height};
+		return { left, top, width, height };
 	}
 
 
 	async function create(
-		activeTab)
+		activeTab,
+		focusSearch)
 	{
 			// close any existing window, in case wasn't already closed
 		await close();
 
 		const targetWindow = await cp.windows.get(activeTab.windowId);
+		const url = `popup.html?${new URLSearchParams({ type, focusSearch })}`;
 		let {left, top, width, height} = calcPosition(targetWindow);
+		let window;
 
-		window = await cp.windows.create({
-			url: "popup.html",
-			type: "popup",
-			setSelfAsOpener: true,
-			left,
-			top,
-			width,
-			height
-		});
-		lastActiveTab = activeTab;
-		isVisible = true;
+		if (type == "window") {
+			window = await cp.windows.create({
+				url,
+				type: "popup",
+				focused: true,
+				setSelfAsOpener: true,
+				left,
+				top,
+				width,
+				height
+			});
+			windowID = window.id;
+		} else {
+				// first add the tab to the last unfocused window, then show()
+				// move that tab to a new popup window
+			const lastWindow = await getLastWindow();
+			const tab = await cp.tabs.create({
+				url,
+				active: false,
+				windowId: lastWindow.id,
+				index: lastWindow.tabs.length + 1
+			});
 
-		const {width: innerWidth, height: innerHeight} = window.tabs[0];
+			tabID = tab.id;
+			window = await show(activeTab);
+		}
+
+		const [popupTab] = window.tabs;
+		const {width: innerWidth, height: innerHeight} = popupTab;
 		const widthDelta = PopupInnerWidth - innerWidth;
 		const heightDelta = PopupInnerHeight - innerHeight;
 
@@ -70,6 +103,9 @@ define([
 			await cp.windows.update(window.id, { width, height });
 		}
 
+		lastActiveTab = activeTab;
+		isVisible = true;
+
 		return window;
 	}
 
@@ -77,13 +113,31 @@ define([
 	async function show(
 		activeTab)
 	{
-		if (window) {
-			const targetWindow = await cp.windows.get(activeTab.windowId);
-			let {left, top} = calcPosition(targetWindow);
+		const targetWindow = await cp.windows.get(activeTab.windowId);
+		let {left, top, width, height} = calcPosition(targetWindow);
 
-			await cp.windows.update(window.id, { focused: true, left, top });
-			lastActiveTab = activeTab;
-			isVisible = true;
+		lastActiveTab = activeTab;
+		isVisible = true;
+
+		if (type == "window" && windowID) {
+			return cp.windows.update(windowID, { focused: true, left, top });
+		} else if (type == "tab" && tabID) {
+				// create a popup window with the tab that's hiding in another
+				// window, instead of passing in a URL
+			const window = await cp.windows.create({
+				type: "popup",
+				tabId: tabID,
+				focused: true,
+				setSelfAsOpener: true,
+				left,
+				top,
+				width,
+				height
+			});
+
+			windowID = window.id;
+
+			return window;
 		}
 	}
 
@@ -91,7 +145,9 @@ define([
 	async function hide(
 		unfocus)
 	{
-		if (window) {
+		isVisible = false;
+
+		if (type == "window" && windowID) {
 			const options = {
 				left: OffscreenX,
 				top: OffscreenY
@@ -101,23 +157,38 @@ define([
 				options.focused = false;
 			}
 
-			await cp.windows.update(window.id, options);
-			isVisible = false;
+			return cp.windows.update(windowID, options);
+		} else if (type == "tab" && tabID) {
+			const lastWindow = await getLastWindow();
+
+			try {
+				return cp.tabs.move(tabID, {
+					windowId: lastWindow.id,
+					index: -1
+				});
+			} catch (e) {
+				console.log(e);
+			}
 		}
 	}
 
 
 	async function close()
 	{
-		if (window) {
+		if (tabID) {
 			try {
-				await cp.windows.remove(window.id);
-			} catch (error) {
-				console.error(error);
-			}
+				await cp.tabs.remove(tabID);
+			} catch (e) {}
 		}
 
-		window = null;
+		if (windowID) {
+			try {
+				await cp.windows.remove(windowID);
+			} catch (e) {}
+		}
+
+		windowID = 0;
+		tabID = 0;
 		isVisible = false;
 	}
 
@@ -127,13 +198,25 @@ define([
 		show,
 		hide,
 		close,
+		get type() {
+			return type;
+		},
+		set type(value) {
+			if (type !== value) {
+				close();
+			}
+
+			type = value;
+		},
 		get id() {
-			return window && window.id;
+			return windowID;
 		},
 		get isOpen() {
-			return window
-				? chrome.extension.getViews({ windowId: window.id }).length == 1
-				: false;
+			const query = type == "window"
+				? { windowId: windowID }
+				: { tabId: tabID };
+
+			return chrome.extension.getViews(query).length > 0;
 		},
 		get isVisible() {
 			return isVisible;
