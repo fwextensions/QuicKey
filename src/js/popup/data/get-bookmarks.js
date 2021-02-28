36201ee01926d68d4f33e31bb1ad6ed63ba15233
John Dunning <fw@johndunning.com>
2021-02-27 17:58:31 -0800

    Show bookmark folder path in bookmark titles

--- src/js/popup/data/get-bookmarks.js
+++ src/js/popup/data/get-bookmarks.js
@@ -5,23 +5,43 @@ define([
 	addURLs,
 	cp
 ) {
+	const PathConnector = " / ";
+
+
 	let bookmarks = [];
 	let urls = {};
+	let path = [];
 
 
 	function processNodes(
 		nodes)
 	{
 		nodes.forEach(node => {
-			const {url, children} = node;
+			const {url, children, title, parentId} = node;
 
 				// don't return any duplicate URLs
 			if (url && !urls[url]) {
 				addURLs(node);
 				urls[url] = true;
+
+				if (path.length) {
+					node.title = path.join(PathConnector) + PathConnector + title;
+				}
+
 				bookmarks.push(node);
 			} else if (children) {
+				let titlePushed = false;
+
+				if (parentId && parentId !== "0" && title) {
+					path.push(node.title);
+					titlePushed = true;
+				}
+
 				processNodes(children);
+
+				if (titlePushed) {
+					path.pop();
+				}
 			}
 		});
 	}
@@ -31,6 +51,7 @@ define([
 	{
 		bookmarks = [];
 		urls = {};
+		path = [];
 
 		return cp.bookmarks.getTree()
 			.then(bookmarkNodes => {
