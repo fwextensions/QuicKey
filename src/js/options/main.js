66e8de6e1ce2d05f389172c0a93b3744c103f6c0
John Dunning <fw@johndunning.com>
2024-09-01 15:24:35 -0700

    Move setup of stdout connection to error-handler.js
    Comment it out for now and force DEBUG to be false.
    Tweak the section list styling in options.

--- src/js/options/main.js
+++ src/js/options/main.js
@@ -3,12 +3,7 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import {setup} from "goober";
 import {shouldForwardProp} from "goober/should-forward-prop";
-import stdout from "@/lib/stdout";
 import OptionsAppContainer from "./app-container";
-
-if (globalThis.DEBUG) {
-	stdout("diohkfkdnhkijfjdjcmdbpemmapfgpgg");
-}
 
 setup(React.createElement, undefined, undefined,
 	shouldForwardProp((prop) => !/left|right|top|bottom|width|height|tabWidth|navigating|enabled/.test(prop)));
